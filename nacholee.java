61620af16918ceb7d97ce510ba633e585ce54aee
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-02-09 09:27:23 -0500

    Creacion de un metodo para sumar dos numeros

--- nacholee.java
+++ nacholee.java
@@ -10,6 +10,11 @@ public class nacholee {
         n1 = sc.nextInt();
          System.out.println("Ingrese el numero 2: ");
         n2 = sc.nextInt();
-        System.out.println("La suma de los numeros es: " + (n1+n2));
+        System.out.println(SumaNumeros(n1, n2));
     }
+
+    public static int SumaNumeros(int  n1, int n2){
+         return n1 + n2;
+    }
+            
 }
